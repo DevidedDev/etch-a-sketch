0cac24ffdf2af78853431752a7595125c2ea7f40
DevidedDev <108572163+DevidedDev@users.noreply.github.com>
2023-08-09 18:54:01 +0200

    Add tool optionsfunctionality

--- script.js
+++ script.js
@@ -10,11 +10,65 @@
 //create function that adds functionality to each block
     //event listener => if clicked, change color
 
+let selectedColor= "black";
+let selectedOption= "brush";
+
+const clrPicker = document.getElementById("color-picker")
+const toolBtn = document.querySelectorAll("[data-tool")
+
 const sketchPad = document.getElementById("sketch-pad");
-let selectedColor = "red";
+
+toolBtn.forEach( (button) => {
+    button.addEventListener("click", changeToolSelection);
+    
+});
+
+clrPicker.addEventListener("change", changeColor);
+
+function displaySelectedTool(selectedTool){
+    toolBtn.forEach( (button)=>{
+        button.classList.remove("btn-selected");
+        if(selectedTool === button.getAttribute("data-tool")){
+            button.classList.add("btn-selected");
+        }
+    });
+
+}
+
+function changeColor(){
+    if(toolBtn.item(0).getAttribute("class") ===  "btn-selected"){
+        selectedColor = clrPicker.value;
+        
+    }
+}
+
+function changeToolSelection(){
+    const selectedTool = (this.getAttribute("data-tool"));
+
+    displaySelectedTool(selectedTool);
+    console.log(selectedTool);
+    switch(selectedTool){
+        case "solid-color":
+            changeColor();
+            break;
+
+        case "shadow":
+            selectedColor = "rgb(255,255,255)";
+            selectedOption = "shadow";
+            break;
+        case "rainbow":
+            selectedOption = "rainbow";
+            break;
+        case "erasor":
+            selectedOption = "erasor"
+            selectedColor = "white";
+            break;
+    }
+}
+
+
 
 function createGrid(size){
-
     for(let i = 0; i < size; i++){
         const sketchRow = document.createElement("div");
         sketchRow.classList.add("sketch-row");
@@ -31,6 +85,43 @@ function createGrid(size){
 
 createGrid(16);
 
+function generateRandNum(start, end){
+    
+    return Math.floor(Math.random()*(end-start+1)+start)
+}
+
+function getRainbowClr(){
+    const red= generateRandNum(0,255);
+    const green = generateRandNum(0,255);
+    const blue =  generateRandNum(0,255);
+    return `rgb(${red},${green},${blue})`;
+}
+
+function getShadowClr(){
+    let shadowClr = (selectedColor.split(","))[1];
+    console.log(shadowClr);
+    shadowClr -= Math.floor(255/10);
+    if(shadowClr < 0){
+        shadowClr = 255;
+    }
+    
+    
+    return `rgb(${shadowClr},${shadowClr},${shadowClr})`;
+}
+
 function changeBgClr(){
+    switch(selectedOption){
+        case "shadow":
+            selectedColor = getShadowClr();
+            break;  
+        case "rainbow":
+            selectedColor = getRainbowClr();
+            console.log(selectedColor);
+            break;
+        case "erasor":
+        case "brush":
+            break;
+    }
     this.style.backgroundColor = selectedColor;
+
 }
